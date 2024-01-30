9b69080e800e00db53d229f4c2c55fbe30d47f1d
Carl Turechek <carl@siteraiser.com>
2024-01-30 18:51:13 -0500

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,8 +1,5 @@
-<?php /*
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-*/
+<?php 
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 require_once('dbconn.php');
