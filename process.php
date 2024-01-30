0b1becdbc54cb8945d5aad5a3d0732de032e3652
Carl Turechek <carl@siteraiser.com>
2024-01-30 18:52:21 -0500

    Update process.php

--- process.php
+++ process.php
@@ -1,32 +1,13 @@
 <?php
 
 require_once('dbconn.php');
-require_once('walletapi.php');
-class UUID {
-	//Thank you commenters in the PHP docs
-  public static function v4() {
-    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+//require_once('walletapi.php');
+require_once($_SERVER["DOCUMENT_ROOT"]."/classes/UUID.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/classes/DeroApi.php");
 
-      // 32 bits for "time_low"
-      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-
-      // 16 bits for "time_mid"
-      mt_rand(0, 0xffff),
-
-      // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 4
-      mt_rand(0, 0x0fff) | 0x4000,
-
-      // 16 bits, 8 bits for "clk_seq_hi_res",
-      // 8 bits for "clk_seq_low",
-      // two most significant bits holds zero and one for variant DCE1.1
-      mt_rand(0, 0x3fff) | 0x8000,
-
-      // 48 bits for "node"
-      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-    );
-  }
-}
+//Instantiate required classes
+$UUID = new UUID;	
+$DeroApi = new DeroApi;	
 
 function getInstalledTime($pdo){
 
@@ -119,7 +100,7 @@ function makeTxObject($pdo,$entry){
 //check responses to ensure they went through, if not mark as not processed 
 function unConfirmedTxs($pdo){
 
-	$stmt=$pdo->prepare("SELECT * FROM responses WHERE confirmed = '0'");
+	$stmt=$pdo->prepare("SELECT DISTINCT txid,time_utc FROM responses WHERE confirmed = '0'");
 	$stmt->execute([]);		
 	if($stmt->rowCount()==0){
 		return [];
@@ -153,23 +134,57 @@ function markResAsConfirmed($pdo,$txid){
 		return false;
 	}
 }
-function markIncAsNotProcessed($pdo,$id){
 
-	$query='UPDATE incoming SET 
-		processed=:processed
-		WHERE id=:id';	
-	
-	$stmt=$pdo->prepare($query);
-	$stmt->execute(array(
-		':processed'=>0,
-		':id'=>$id));				
-				
+
+function getTXCollection($pdo,$txid){
+
+	$stmt=$pdo->prepare("SELECT incoming_id FROM responses WHERE txid = ?");
+	$stmt->execute([$txid]);		
 	if($stmt->rowCount()==0){
+		return [];
+	}
+	
+	return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function markIncAsNotProcessed($pdo,$txid){
+	$incoming_ids = getTXCollection($pdo,$txid);
+	$ids =[];
+	foreach($incoming_ids as $incoming_ids){
+		$ids[] = $incoming_ids['incoming_id'];
+	}
+	
+	$ids = implode(",",$ids);
+	$result =$pdo->query("UPDATE incoming SET processed = '0' WHERE id IN($ids)");
+	if($result !== false && $result->rowCount() > 0){		
+		return true;
+	}else{	
 		return false;
 	}
 }
+
+function getConfirmedInc($pdo,$txid){
+
+	$stmt=$pdo->prepare("
+	SELECT * FROM responses 
+	INNER JOIN incoming ON responses.incoming_id = incoming.id 
+	WHERE responses.txid = ?");
+	$stmt->execute([$txid]);		
+	if($stmt->rowCount()==0){
+		return [];
+	}
+	
+	return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
+
 $messages = [];
 $unConfirmed = unConfirmedTxs($pdo);
+
+
+
+$confirmed_txns=[];
 //go through the resposes that haven't been confirmed.
 foreach($unConfirmed as $out_message){
 	//make sure the response is at least one block old before checking.
@@ -179,34 +194,46 @@ foreach($unConfirmed as $out_message){
 	$time_utc = $given->format("Y-m-d H:i:s");
 	
 	if($out_message['time_utc'] < $time_utc){
-		$check_transaction_result = check_transaction($ip,$port,$user,$pass,$out_message['txid']);
+		$check_transaction_result = $DeroApi->getTransferByTXID($out_message['txid']);
 		$check_transaction_result = json_decode($check_transaction_result);
 
 		//succesfully confirmed 
 		if(!isset($check_transaction_result->errors) && isset($check_transaction_result->result)){		
 			markResAsConfirmed($pdo,$out_message['txid']);	
-			$messages[] = $out_message['type']." confirmed with txid:".$out_message['txid'];	
-			//send post message to your web api here... 
-			if($out_message['out_message_uuid'] == 1){
-				//$customAPIAddress = $out_message['out_message'];	
-				
-			}
+			$confirmed_txns[] = $out_message['txid'];
+			
+			//$messages[] = $out_message['type']." confirmed with txid:".$out_message['txid'];	
+			
 			
 		}else{
-			//delete the response reccord and set the incoming to not processed.
-			removeResponse($pdo,$out_message['txid']);
-			markIncAsNotProcessed($pdo,$out_message['incoming_id']);		
+			//set the incoming to not processed and delete the response reccord. 			
+			markIncAsNotProcessed($pdo,$out_message['txid']);	
+			removeResponse($pdo,$out_message['txid']);	
 		}
 	}
 }
 
+foreach($confirmed_txns as $txid){
+	$confirmed_incoming = getConfirmedInc($pdo,$txid);
 
+	foreach($confirmed_incoming as $record){
+		$messages[] = $record['type']." confirmed with txid:".$record['txid'];	
+		
+		//send post message to your web api here... 
+		if($record['out_message_uuid'] == 1){
+			//$customAPIAddress = $out_message['out_message'];	
+				
+		}
+		
+		
+	}
+}
 
 
 $errors = [];
 //$notProcessed=[];
 //Get transfers and save them if they are new and later than the db creation time.	
-$export_transfers_result =	export_transfers($ip,$port,$user,$pass);
+$export_transfers_result = $DeroApi->getTransfers();
 $export_transfers_result = json_decode($export_transfers_result);
 if($export_transfers_result === NULL){
 	$errors[] = "Wallet Connection Error.";
@@ -238,7 +265,7 @@ function unprocessedTxs($pdo){
 function getIAsettings($pdo,$tx){
 
 	$stmt=$pdo->prepare("
-	SELECT * FROM i_addresses 
+	SELECT *, i_addresses.comment AS ia_comment FROM i_addresses 
 	INNER JOIN products ON i_addresses.product_id = products.id  WHERE i_addresses.ask_amount = ? AND i_addresses.port = ? AND i_addresses.status = '1'");
 	$stmt->execute([$tx['amount'],$tx['port']]);		
 	if($stmt->rowCount()==0){
@@ -312,68 +339,76 @@ if($new !== false){
 
 
 $type = '';	
-$UUID = new UUID;	
-foreach($notProcessed as $tx){
+
+
+$transfer_list = [];
+
+foreach($notProcessed as &$tx){
 	
 	$settings = getIAsettings($pdo,$tx);
-
+	
+	
+	$transfer=[];
 	if($settings !== false){
-		$respond_amount = $settings['respond_amount'];
-		$address = $tx['buyer_address'];
+		//Send Response to buyer
+		$transfer['respond_amount'] = $settings['respond_amount'];
+		$transfer['address'] = $tx['buyer_address'];	
 		
 		if($settings['out_message_uuid'] == 1){
-			//$customAPIAddress = $settings['out_message'];			
 			$settings['out_message'] = $UUID->v4();
-			//No longer safe to assume the transaction went through... 
-			//send a curl request to endpoint on line: 190
-			//$customAPIAddress
 		}
 		
-		$out_message = $settings['out_message'];
-		$responseTXID = '';
-		//Send Response to buyer
-		$payload_result = payload($ip, $port, $user, $pass, $respond_amount, $address, $scid, $out_message);
-		$payload_result = json_decode($payload_result);
-		
-		//$payload_result ='';
-			
-		//Ensure that the response transfer is successful
-		if($payload_result != null && $payload_result->result){
-			$type="sale";
-			$responseTXID = $payload_result->result->txid;	
-
-			$messages[] = "Responding to Transaction for \"{$settings['comment']}\",  Ask Amount:{$tx['amount']}, Port:{$tx['port']} with Out Message:\"{$settings['out_message']}\" and Respond Amount:{$settings['respond_amount']} txid:({$tx['txid']}) response txid:($responseTXID)";
-		}else{
-			$errors[] = "Error Processing Transaction {$tx['txid']}";
-		}
-		
+		$transfer['out_message'] = $settings['out_message'];
+		$transfer['scid'] = "0000000000000000000000000000000000000000000000000000000000000000";
+		$transfer_list[]=(object)$transfer;
+		//update unprocessed array
+		$tx['ia_comment'] = $settings['ia_comment'];
+		$tx['respond_amount'] = $transfer['respond_amount'];
+		$tx['out_message'] = $transfer['out_message'];
+		$tx['type'] = "sale";
+		$tx['out_message_uuid'] = $settings['out_message_uuid'];
 	}else{
 		//No mathcing products / I. Addresses found
 		//Send Refund to buyer
-		$respond_amount = $tx['amount'];
-		$address = $tx['buyer_address'];
-		$out_message = "Integrated Address Inactive.";	
 		
-		$payload_result = payload($ip, $port, $user, $pass, $respond_amount, $address, $scid, $out_message);
-		$payload_result = json_decode($payload_result);
-		
-		//$payload_result ='';
-					
-		$responseTXID = '';
-		//Ensure that the response transfer is successful
-		if($payload_result != null && $payload_result->result){
-			$type="refund";
-			$responseTXID = $payload_result->result->txid;
-			
-
-			$messages[] = "Initiating Refund for Ask Amount:{$tx['amount']} and Port:{$tx['port']} to $address!";
-		}else{
-			$errors[] = "Error Refunding Transaction {$tx['txid']}";
-		}
+		$transfer['respond_amount'] = $tx['amount'];
+		$transfer['address'] = $tx['buyer_address'];	
+		$transfer['out_message'] =  "Integrated Address Inactive.";	
+		$transfer['scid'] = "0000000000000000000000000000000000000000000000000000000000000000";
+		$transfer_list[]=(object)$transfer;	
+		//update unprocessed array
+		$tx['respond_amount'] =  $tx['amount'];
+		$tx['out_message'] = $transfer['out_message'];
+		$tx['type'] = "refund";
+		$tx['out_message_uuid'] = '';
 	} 
-	if(empty($errors)){
-		$result = markAsProcessed($pdo,$tx['txid']);
+	
+}	
+
+unset($tx);
+
+
+
+$responseTXID='';
+/*die();
+*/
+if(!empty($transfer_list)){
+	$payload_result = $DeroApi->transfer($transfer_list);
+	$payload_result = json_decode($payload_result);
+
+	if($payload_result != null && $payload_result->result){
+		$responseTXID = $payload_result->result->txid;
+	}else{
+		$errors[] = "Transfer Error";
+	}
+}
+
+
+if(empty($errors) && $responseTXID !== ''){
+	foreach($notProcessed as $tx){
 		
+		$result = markAsProcessed($pdo,$tx['txid']);
+			
 		$given = new DateTime();
 		$given->setTimezone(new DateTimeZone("UTC"));	
 		$time_utc = $given->format("Y-m-d H:i:s");
@@ -382,22 +417,26 @@ foreach($notProcessed as $tx){
 			$response = (object)[
 			"incoming_id"=>$tx['id'],
 			"txid"=>$responseTXID,
-			"type"=>$type,
-			"buyer_address"=>$address,
-			"out_amount"=>$respond_amount,
+			"type"=>$tx['type'],
+			"buyer_address"=>$tx['buyer_address'],
+			"out_amount"=>$tx['respond_amount'],
 			"port"=>$tx['port'],
-			"out_message"=>$out_message,
-			"out_message_uuid"=>($settings !==false?$settings['out_message_uuid']:''),
+			"out_message"=>$tx['out_message'],
+			"out_message_uuid"=>$tx['out_message_uuid'],
 			"time_utc"=>$time_utc
 			];
 			
 			
 			insertResponse($pdo,$response);
+			$messages[] = "{$tx['type']} response initiated". ($tx['type'] == 'sale' ? ' for "'.$tx['ia_comment'].'"' : '') . ".";
 		}
 	}
-	
-		
-}	
+}
+
+
+
+
+
 
 
 if(empty($errors)){
