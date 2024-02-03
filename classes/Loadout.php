00a8cf6123ffce67925815d2266b8538e523dd48
Carl Turechek <carl@siteraiser.com>
2024-02-02 23:51:24 -0500

    Update Loadout.php

--- classes/Loadout.php
+++ classes/Loadout.php
@@ -1,13 +1,10 @@
 <?php 
 class Loadout extends App {  	
 	function load(){
-		require_once('system/dbtablesetup.php');
+
 		$this->loadModel('loadoutModel');
 		$transactions = $this->loadoutModel->getTransactionList();		
-		
-	//	foreach ($results as &$tx){
-	//		$product['iaddress'] = $this->InitializeModel->getIAddresses($product['id']);			
-	//	}		
+				
 		return ["transactions"=>$transactions];		
 	}
 }
