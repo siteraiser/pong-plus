7b523e54e2d5f90908d20dc0dc6f82c0b801e32c
Carl Turechek <carl@siteraiser.com>
2024-02-03 16:34:10 -0500

    Update loadOutModel.php

--- models/loadOutModel.php
+++ models/loadOutModel.php
@@ -2,10 +2,12 @@
 class loadoutModel extends App {  
 	function getTransactionList(){
 		$stmt=$this->pdo->prepare("
-		SELECT i.*, ia.* , res.*
+		SELECT i.*, ia.* , res.*, p.*
 		FROM incoming as i 
+		LEFT JOIN products as p 
+		ON (i.for_product_id = p.id)
 		LEFT JOIN i_addresses as ia 
-		ON (i.amount = ia.ask_amount AND i.port = ia.port)
+		ON (i.amount = ia.ask_amount AND i.port = ia.port AND p.id = ia.product_id)
 		INNER JOIN responses as res 
 		ON (i.id = res.incoming_id) 
 		WHERE res.type = 'sale'
